7560d7fc2a061878c33f6412e0a2e84d032370f0
Alstudd <alstonsoares17@gmail.com>
2024-05-03 21:48:13 +0530

    Added hero section

---
+++ src/components/Navbar.tsx
@@ -0,0 +1,7 @@
+export default function Navbar() {
+    return <header className="sticky top-0 bg-background">
+        <div className="max-w-3xl mx-auto flex flex-wrap justify-between">
+
+        </div>
+    </header>
+}
